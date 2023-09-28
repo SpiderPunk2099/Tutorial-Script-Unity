e5a3cdf989f307c2922f8803662f78013e26dd7e
Kiera <11018737@uvu.edu>
2023-09-27 18:43:05 -0600

    tut 5.2

--- tutorial.cs
+++ tutorial.cs
@@ -112,3 +112,17 @@ public class ForLoop : MonoBehaviour
         }
     }
 }
+
+public class DoWhileLoop : MonoBehaviour 
+{
+    void Start()
+    {
+        bool shouldContinue = false;
+        
+        do
+        {
+            print ("Hello World");
+            
+        }while(shouldContinue == true);
+    }
+}
