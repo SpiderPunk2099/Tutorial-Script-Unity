a69128a24fff856d4971746b73e4355e95bd9c65
Kiera <11018737@uvu.edu>
2023-09-27 18:59:06 -0600

    tut 23

--- tutorial.cs
+++ tutorial.cs
@@ -709,3 +709,28 @@ public class MovementControls : MonoBehaviour
         transform.Rotate(Vector3.up * turnMovement);
     }
 }
+
+public class UsingInstantiate : MonoBehaviour
+{
+    public Rigidbody rocketPrefab;
+    public Transform barrelEnd;
+    
+    
+    void Update ()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody rocketInstance;
+            rocketInstance = Instantiate(rocketPrefab, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
+            rocketInstance.AddForce(barrelEnd.forward * 5000);
+        }
+    }
+}
+
+public class RocketDestruction : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy (gameObject, 1.5f);
+    }
+}
