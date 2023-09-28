8829f188756f11a8df76620276e92024f0e614dc
Kiera <11018737@uvu.edu>
2023-09-27 19:00:04 -0600

    tut 25

--- tutorial.cs
+++ tutorial.cs
@@ -749,3 +749,37 @@ public class Arrays : MonoBehaviour
         }
     }
 }
+
+public class InvokeScript : MonoBehaviour 
+{
+    public GameObject target;
+    
+    
+    void Start()
+    {
+        Invoke ("SpawnObject", 2);
+    }
+    
+    void SpawnObject()
+    {
+        Instantiate(target, new Vector3(0, 2, 0), Quaternion.identity);
+    }
+}
+
+public class InvokeRepeating : MonoBehaviour 
+{
+    public GameObject target;
+    
+    
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", 2, 1);
+    }
+    
+    void SpawnObject()
+    {
+        float x = Random.Range(-2.0f, 2.0f);
+        float z = Random.Range(-2.0f, 2.0f);
+        Instantiate(target, new Vector3(x, 2, z), Quaternion.identity);
+    }
+}
