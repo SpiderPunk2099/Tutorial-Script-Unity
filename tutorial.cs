0432cb1e8fee027e02ccbc6101dbdd67f6b8966f
Kiera <11018737@uvu.edu>
2023-09-27 18:43:25 -0600

    tut 5.3

--- tutorial.cs
+++ tutorial.cs
@@ -126,3 +126,20 @@ public class DoWhileLoop : MonoBehaviour
         }while(shouldContinue == true);
     }
 }
+
+public class ForeachLoop : MonoBehaviour 
+{   
+    void Start () 
+    {
+        string[] strings = new string[3];
+        
+        strings[0] = "First string";
+        strings[1] = "Second string";
+        strings[2] = "Third string";
+        
+        foreach(string item in strings)
+        {
+            print (item);
+        }
+    }
+}
