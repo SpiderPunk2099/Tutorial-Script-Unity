497cd5667401859fef805da3ef971515887c35b3
Kiera <11018737@uvu.edu>
2023-09-27 18:42:42 -0600

    tut 5

--- tutorial.cs
+++ tutorial.cs
@@ -98,3 +98,17 @@ public class IfStatements : MonoBehaviour
         }
     }
 }
+
+public class ForLoop : MonoBehaviour
+{
+    int numEnemies = 3;
+    
+    
+    void Start ()
+    {
+        for(int i = 0; i < numEnemies; i++)
+        {
+            Debug.Log("Creating enemy number: " + i);
+        }
+    }
+}
