13be281e1c4d6160b33bdb54f4c9e7c5cfc08d17
Kiera <11018737@uvu.edu>
2023-09-27 18:41:02 -0600

    tut 3

--- tutorial.cs
+++ tutorial.cs
@@ -41,3 +41,21 @@ public class VariablesAndFunctions : MonoBehaviour
         return result;
     }
 }
+
+public class BasicSyntax : MonoBehaviour
+{
+    void Start ()
+    {
+        //this line is there to tell me the x position of my object
+
+        /*Hi there!
+         * this is two lines!
+         * */
+        Debug.Log(transform.position.x);
+        
+        if(transform.position.y <= 5f)
+        {
+            Debug.Log ("I'm about to hit the ground!");
+        }
+    }
+}
