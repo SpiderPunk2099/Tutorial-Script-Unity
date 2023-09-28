8d5ec89e859c31dce369020c4727a4025eab44fd
Kiera <11018737@uvu.edu>
2023-09-27 18:40:20 -0600

    tut 2

--- tutorial.cs
+++ tutorial.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
+
 public class ScriptOne : MonoBehaviour
 {
     void Update()
@@ -17,5 +18,26 @@ public class ScriptOne : MonoBehaviour
         {
             GetComponent<Renderer>().material.color = Color.blue;
         }
+    }
+}
+
+
+public class VariablesAndFunctions : MonoBehaviour
+{   
+    int myInt = 5;
+    
+    
+    void Start ()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log (myInt);
+    }
+    
+    
+    int MultiplyByTwo (int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
     }
 }
