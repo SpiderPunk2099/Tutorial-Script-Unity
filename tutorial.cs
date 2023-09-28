a439eeba07176bb08e4404aee9c4f502d47b5b33
Kiera <11018737@uvu.edu>
2023-09-27 18:55:29 -0600

    tut 19

--- tutorial.cs
+++ tutorial.cs
@@ -521,3 +521,42 @@ public class MouseClick : MonoBehaviour
         rb.useGravity = true;
     }
 }
+
+public class UsingOtherComponents : MonoBehaviour
+{
+    public GameObject otherGameObject;
+    
+    
+    private AnotherScript anotherScript;
+    private YetAnotherScript yetAnotherScript;
+    private BoxCollider boxCol;
+    
+    
+    void Awake ()
+    {
+        anotherScript = GetComponent<AnotherScript>();
+        yetAnotherScript = otherGameObject.GetComponent<YetAnotherScript>();
+        boxCol = otherGameObject.GetComponent<BoxCollider>();
+    }
+    
+    
+    void Start ()
+    {
+        boxCol.size = new Vector3(3,3,3);
+        Debug.Log("The player's score is " + anotherScript.playerScore);
+        Debug.Log("The player has died " + yetAnotherScript.numberOfPlayerDeaths + " times");
+    }
+}
+
+using UnityEngine;
+using System.Collections;
+
+public class AnotherScript : MonoBehaviour
+{
+    public int playerScore = 9001;
+}
+
+public class YetAnotherScript : MonoBehaviour
+{
+    public int numberOfPlayerDeaths = 3;
+}
