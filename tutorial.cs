d99ae479bc24c0784943d27b027efc89ce93afa1
Kiera <11018737@uvu.edu>
2023-09-27 18:55:53 -0600

    tut 20

--- tutorial.cs
+++ tutorial.cs
@@ -560,3 +560,20 @@ public class YetAnotherScript : MonoBehaviour
 {
     public int numberOfPlayerDeaths = 3;
 }
+
+public class UsingDeltaTime : MonoBehaviour
+{
+    public float speed = 8f; 
+    public float countdown = 3.0f;
+
+    
+    void Update ()
+    {
+        countdown -= Time.deltaTime;
+        if(countdown <= 0.0f)
+            light.enabled = true;
+        
+         if(Input.GetKey(KeyCode.RightArrow))
+            transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+    }   
+}
